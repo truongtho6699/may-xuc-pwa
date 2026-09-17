4c329cf09fa03c2db1b70fed428d3cf169d14d87
truongtho6699 <truongtho53@gmail.com>
2026-09-17 21:24:40 +0700

    Thêm cấu hình lưu ảnh Google Drive

---
+++ js/config.js
@@ -0,0 +1,7 @@
+window.NGHI_SON_CONFIG = {
+  DRIVE_FOLDER_ID: '1k7GwdI63ABzziT0KuK7Wx3ZRUXmZL6g5',
+  // Sau khi triển khai apps-script/DriveUpload.gs dưới dạng Web App,
+  // dán URL /exec vào đây. Khi để trống, hệ thống tạm dùng Supabase Storage
+  // để không làm gián đoạn vận hành hiện tại.
+  DRIVE_UPLOAD_URL: ''
+};
