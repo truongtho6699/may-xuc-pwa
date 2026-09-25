d37afc863e8f5661605c86877f16ae7f944db070
truongtho6699 <truongtho53@gmail.com>
2026-09-25 11:24:51 +0700

    Nạp bản camera V1 mới nhất

--- js/runtime-fixes.js
+++ js/runtime-fixes.js
@@ -13,7 +13,7 @@ async function gps(){
 (function(){
   const files=[
     ['ns-v1-api-router','js/v1-api-router.js?v=20260925-1'],
-    ['ns-camera-ui-hotfix-direct','js/camera-ui-hotfix.js?v=20260925-1'],
+    ['ns-camera-ui-hotfix-direct','js/camera-ui-hotfix.js?v=20260925-2'],
     ['ns-v1-runtime','js/v1-runtime.js?v=20260925-2']
   ];
   function load(i){
